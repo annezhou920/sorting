01f10e7a1ac008b5ba755957bda00015d75e0618
Anne Zhou <anne.zhou920@gmail.com>
2017-01-12 15:28:51 -0500

    almost completed bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -3,28 +3,51 @@ function swap(num){
   return num;
 }
 
-function comparisonCount(){
-
-}
+// function comparisonCount(num){
+//   totalSwap += num;
+//   return totalSwap;
+// }
 
 function bubbleSort(arr) {
   var swapCount = 0;
-  //
-  // while(){
-    for (var i = 0; i < arr.length - 1; i++){
-      var current = arr[i];
-      var next = arr[i+1];
+  var totalSwap = 0;
 
-      if (next < current){
-        current = next;
-        next = arr[i];
-        swapCount = swap(swapCount);
-      }
-
-    }
+  if (arr.length === 1) {
     return arr;
   }
-// }
 
-[5,2,4,3,1]
-[2,5]
+  for (var i = 0; i < arr.length - 1; i++){
+    if (arr[i] > arr[i+1]){
+      var temp = arr[i];
+      arr[i] = arr[i+1];
+      arr[i+1] = temp;
+
+      swapCount = swap(swapCount);
+
+    }
+  }
+  // totalSwap = comparisonCount(swapCount);
+
+  if (swapCount === 0) {
+    return arr
+  } else {
+    return bubbleSort(arr)
+  }
+}
+
+
+// function BubbleSort(arr){
+//  var swap = true;
+//  while (swap === true){
+//    swap = false;
+//    for (var i = 0; i < arr.length - 1; i++){
+//     if (arr[i] > arr[i+1]){
+//         var temp = arr[i];
+//         arr[i] = arr[i+1];
+//         arr[i+1] = temp;
+//         swap = true;
+//     }
+//    }
+// }
+//   return arr;
+// }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,4 +1,23 @@
+describe("swap", function(){
+  it('takes a number as an argument and returns that number plus one', function () {
+    var randomNum = 3;
+    expect(swap(randomNum)).toEqual(4);
+  });
+});
+
+// describe("comparisonCount", function(){
+//   it('takes a number as an argument and returns the total swap count', function () {
+//     var partialSwapCount= 3;
+//     expect(comparisonCount(partialSwapCount)).toEqual(4);
+//   });
+// });
+
+
 describe('Bubble Sort', function(){
+  beforeEach(function () {
+    spyOn(window, "swap").and.callThrough();
+  });
+
   it('handles an empty array', function(){
     expect( bubbleSort([]) ).toEqual( [] );
   });
@@ -9,5 +28,18 @@ describe('Bubble Sort', function(){
 
   it('handles an array with multiple elements', function(){
     expect( bubbleSort( [5,2,1,4,3]) ).toEqual( [1,2,3,4,5] );
-  })
+  });
+
+  it('calls on swap', function(){
+    var randomArr = [5,2,1,4,3];
+    bubbleSort(randomArr);
+    expect(window.swap).toHaveBeenCalled();
+  });
+
+  it('counts total swaps', function(){
+    var randomArr = [5,2,1,4,3];
+    bubbleSort(randomArr);
+    expect(window.swap.calls.count()).toEqual(6);
+  });
+
 });
